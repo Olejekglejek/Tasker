23dc78e2cf961d9a3cd5e4de23fff1984de1566b
Olejekglejek <negrutaoleg@gmail.com>
2020-12-10 21:05:33 +0000

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Platform, Button } from 'react-native';
+import Header from './Components/Header';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+      const [count, setCount] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+<View style={styles.container}>
+  <Header title='Tasker' />  
+      <Button onPress={setCount({ count+ 1})} title='clickMe'/>
+</View>
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: Platform.OS === "android" ? StatusBar.CurrentHeight : 0,
+    },
+  
 });
+
+export default App;
